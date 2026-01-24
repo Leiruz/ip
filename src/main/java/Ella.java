ce0ca25236bc3239dcb719d6bb4a0b313dc35572
Zuriel Shanley Tanyory <zuriel.shanley@gmail.com>
2026-01-24 17:27:16 +0800

    Level-0: rename, greet, exit

---
+++ src/main/java/Ella.java
@@ -0,0 +1,13 @@
+public class Ella {
+    public static void main(String[] args) {
+        String logo =
+          "███████ ██      ██       █████  \n"
+        + "██      ██      ██      ██   ██ \n"
+        + "█████   ██      ██      ███████ \n"
+        + "██      ██      ██      ██   ██ \n"
+        + "███████ ███████ ███████ ██   ██ \n";
+        System.out.println("Hello I am\n" + logo);
+        System.out.println("What can I do for you?");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
